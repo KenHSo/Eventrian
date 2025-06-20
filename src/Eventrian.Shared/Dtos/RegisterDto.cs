37bfb31ef2f9b58606c015cec2c8c08746b3c96d
Kenneth Haagen Sørensen <Haagen86@gmail.com>
2025-06-21 00:22:19 +0200

    feat: add authentication DTOs
    - Add RegisterDto with validation for user registration
    - Add LoginDto with email, password, and RememberMe support"

---
+++ src/Eventrian.Shared/Dtos/RegisterDto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Eventrian.Shared.Dtos;
+
+public class RegisterDto
+{
+    [Required]
+    [StringLength(50)]
+    public required string FirstName { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public required string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public required string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [StringLength(100, MinimumLength = 6)]
+    public required string Password { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare(nameof(Password), ErrorMessage = "Passwords do not match.")]
+    public required string ConfirmPassword { get; set; }
+
+    [Required]
+    [Phone]
+    public required string PhoneNumber { get; set; }
+}
