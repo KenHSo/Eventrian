47ba87aade27198122f77afd6b12cdb9e29d1cee
Kenneth Haagen Sørensen <Haagen86@gmail.com>
2025-06-21 15:29:45 +0200

    refactor: expand LoginResponseDto with Email

--- src/Eventrian.Shared/Dtos/Auth/LoginResponseDto.cs
+++ src/Eventrian.Shared/Dtos/Auth/LoginResponseDto.cs
@@ -2,6 +2,7 @@
 
 public class LoginResponseDto
 {
+    public string Email { get; set; } = string.Empty;
     public string Token { get; set; } = string.Empty;
     public string Message { get; set; } = string.Empty;
     public List<string>? Errors { get; set; }
